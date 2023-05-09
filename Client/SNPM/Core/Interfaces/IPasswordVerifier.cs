670dce8d27a212b0179ea586255ca71d525d59f9
Przemek <mrepicham@gmail.com>
2023-05-09 21:02:05 +0200

    Initialize PasswordVerifier

---
+++ Client/SNPM/Core/Interfaces/IPasswordVerifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SNPM.Core.Interfaces
+{
+    [Flags]
+    public enum PasswordQuality
+    {
+        None = 0,
+        InvalidLength, 
+        NotEnoughWordGroups, // Uppercase, lowercase etc
+        DictionaryFailed, // Checked remotely
+        ContainsCommonWord // Password contains username, service name etc
+    }
+    internal interface IPasswordVerifier
+    {
+        public Task<PasswordQuality> VerifyPassword(string password);
+
+    }
+}
---
+++ Client/SNPM/MVVM/Models/PasswordVerifier.cs
@@ -0,0 +1,80 @@
+﻿using SNPM.Core.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace SNPM.MVVM.Models
+{
+    [Flags]
+    public enum CharacterGroup
+    {
+        None,
+        Lowercase,
+        Uppercase,
+        Numeric,
+        Special
+    }
+
+    public enum HashType
+    {
+        MD5,
+        SHA256
+    }
+
+    internal class PasswordVerifier : IPasswordVerifier
+    {
+        public delegate bool RemoteDictionaryVerifier(string passwordHash);
+        
+        private static readonly CharacterGroup DefaultGroups = 
+            CharacterGroup.Lowercase | 
+            CharacterGroup.Uppercase | 
+            CharacterGroup.Numeric | 
+            CharacterGroup.Special;
+
+        private RemoteDictionaryVerifier RemoteVerifier;
+        private HashType RemoteHashType;
+        
+
+        public PasswordVerifier(RemoteDictionaryVerifier verifier, HashType hashType)
+        {
+            RemoteVerifier = verifier;
+            RemoteHashType = hashType;
+        }
+
+        public async Task<PasswordQuality> VerifyPassword(string password)
+        {
+            var dictionaryVerificationTask = VerifyDictionary(password);
+
+            var lengthVerification = VerifyLength(password);
+            var groupVerification = VerifyWordGroups(password, null);
+
+            var dictionaryVerification = await dictionaryVerificationTask;
+
+            return (lengthVerification ? PasswordQuality.None : PasswordQuality.InvalidLength) |
+                   (groupVerification ? PasswordQuality.None : PasswordQuality.NotEnoughWordGroups) |
+                   (dictionaryVerification ? PasswordQuality.None : PasswordQuality.DictionaryFailed);
+        }
+
+        private bool VerifyLength(string password)
+        {
+            return false;
+        }
+
+        private bool VerifyWordGroups(string password, CharacterGroup? requiredGroups)
+        {
+            if (requiredGroups == null)
+            {
+                requiredGroups = DefaultGroups;
+            }
+
+            return false;
+        }
+
+        private async Task<bool> VerifyDictionary(string password)
+        {
+            // TODO: (Przemek) Implement after server has dictionary verification implemented.
+            await Task.Delay(1);
+
+            return false;
+        }
+    }
+}
